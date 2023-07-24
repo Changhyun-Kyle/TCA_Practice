2e3956673da27081ac56c00ca1541d7a43167377
Changhyun Kang <changhyun.kyle@gmail.com>
2023-07-24 18:04:20 +0900

    Initial commit

---
+++ TCA-Practice/TCA-Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA-Practice
+//
+//  Created by 강창현 on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
